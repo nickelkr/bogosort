08b4fa7401186aa2a20f2ae5849b0f6e13840467
Kyle Nickel <nickelkr@protonmail.com>
2021-05-04 21:36:59 -0400

    Basic index page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let index = warp::path::end().map(|| "Bogosort home");
+
+    println!("Starting server at: 0.0.0.0:3121");
+    warp::serve(index)
+        .run(([0, 0, 0, 0], 3131))
+        .await;
 }
